34d7e338bd92bfebb346083135f3cab4e9575ef5
Sophia Price <pricesophia.dev@gmail.com>
2019-01-21 13:54:38 +0000

    console logs, warn, error, table & time

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-alert('Hello World');
+// Log to Console
+const greeting = "Hello World";
+
+console.clear();
+
+console.log(greeting);
+console.log(123);
+console.log(true);
+
+console.table({ a: 1, b: 2 });
+
+console.error("This is an error");
+
+console.warn("This is a warning");
+
+console.time("checkTime");
+console.log(greeting);
+console.log(greeting);
+console.log(greeting);
+console.log(greeting);
+console.log(greeting);
+console.timeEnd("checkTime");

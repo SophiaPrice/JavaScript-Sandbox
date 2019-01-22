38a79f7648003b1bada4ff0b40a0bb2c5441ce7e
Sophia Price <pricesophia.dev@gmail.com>
2019-01-22 13:59:23 +0000

    Data Type in JS

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-alert('Hello World');
+// PRIMITIVE TYPES
+// String
+const name = "Jane Doe";
+// Number
+const age = 45;
+// Boolean
+const hasKids = true;
+// Null
+const car = null;
+// Undefined
+let test;
+// Symbol
+const sym = Symbol();
+
+console.log(typeof sym);
+
+// REFERNCE TYPES - Objects
+// Array
+const hobbies = ["movies", "music"];
+// Object Literal
+const address = {
+  city: "Boston",
+  state: "Ma"
+};
+// Date
+const today = new Date();
+console.log(today);
+
+console.log(typeof today);

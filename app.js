cfacea5f43813140102921a76278ad4709c3b202
Sophia Price <pricesophia.dev@gmail.com>
2019-01-30 21:56:31 +0000

    function declarations & expressions

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-alert('Hello World');
+// FUNCTION DECLARATIONS
+function greet(fName = "Sophia" /* ES6 Default */) {
+  // ES5 Default
+  if (typeof fName === "undefined") {
+    fName = "Sophia";
+  }
+
+  return "Hello " + fName;
+}
+
+console.log(greet());
+
+// FUNCTION EXPRESSIONS
+const square = function(x) {
+  return x * x;
+};
+
+console.log(square(8));
+
+// IMMEDIATELY INVOKABLE EXPRESSIONS - IIFEs
+(function() {
+  console.log("IIFE Ran..");
+})();
+
+// PROPERTY METHODS
+const todo = {
+  add: function() {
+    console.log("Add todo..");
+  },
+  edit: function(id) {
+    console.log(`edit todo ${id}`);
+  }
+};
+
+todo.delete = function() {
+  console.log("Delete todo...");
+};
+
+todo.add();
+todo.edit(22);
+todo.delete();
